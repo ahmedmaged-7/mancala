6b444cddd912bb51d85c64f2beaedb1e79e92b5c
Mohamed Essam <imessa007@gmail.com>
2021-06-10 04:49:25 +0200

    Adding save and load options.

--- NOde.py
+++ NOde.py
@@ -1,14 +1,26 @@
-import random
-
+import threading
+import time
 board = [4, 4, 4, 4, 4, 4,
          4, 4, 4, 4, 4, 4]
 
+"""import sys
+def ono ():
+    while True:
+        char = sys.stdin.read(1)
+        print ("You pressed: "+char)
+        char = sys.stdin.read(1)
 
-# player 0 human
-# player 1 AI
+t1 = threading.Thread(target=ono)
+t1.start()
+"""
+
+#player 0 humano
+
+#player 1 AI
 class Node:
-    def __init__(self, data, player, score, depth):
+    def __init__(self, data, player, score, depth=0):
         self.children = []
+        self.NextMove=None
         self.alpha = float('-inf')
         self.beta = float('inf')
         self.score = score  # list of 2 contains score of each player
@@ -16,65 +28,77 @@ class Node:
         self.evaluateValue = None
         self.player = player
         self.depth = depth
-        self.cutoff = False
-
-    def AlphaBeta(self, alpha, beta):
-        if self.evaluateValue != None:
-            return self.evaluateValue, self.alpha, self.beta
-        self.alpha = alpha
-        self.beta = beta
+        self.cutoff=False
+    def AlphaBeta(self,alpha,beta):
+        if   self._IsLeaf(self):
+            return self.evaluateValue,self.alpha,self.beta
+        self.NextMove=self.children[0]
+        self.alpha=alpha
+        self.beta=beta
         for l in self.children:
-            value, alphaT, betaT = l.AlphaBeta(self.alpha, self.beta)
+            value,alphaT,betaT = l.AlphaBeta(self.alpha,self.beta)
             if self.player == 1:
-                if value == None: value = float('-inf')
-                if betaT == float('inf'): betaT = float('-inf')
-                self.alpha = max(value, self.alpha, betaT)
+                if value==None:value=float('-inf')
+                if betaT==float('inf'):betaT=float('-inf')
+                self.alpha = max(value, self.alpha,betaT)
+                if l.beta > self.NextMove.beta :self.NextMove = l
             else:
-                if value == None: value = float('inf')
-                if alphaT == float('-inf'): alphaT = float('inf')
-                self.beta = min(value, self.beta, alphaT)
-            if self.alpha >= self.beta:
-                self.cutoff = True
-                print("cutoff")
-                break
-                # return self.evaluateValue,self.alpha,self.beta
-        return self.evaluateValue, self.alpha, self.beta
+                if value==None:value=float('inf')
+                if alphaT==float('-inf'):alphaT=float('inf')
+                self.beta = min(value, self.beta,alphaT)
+                if l.alpha < self.NextMove.alpha :self.NextMove = l
 
+            if self.alpha >= self.beta :
+                self.cutoff = True
+                #print("cutoff")
+                break
+                #return self.evaluateValue,self.alpha,self.beta
+        return self.evaluateValue,self.alpha,self.beta
     def insert(self):
-        if self.depth == 2:
-            self.evaluateValue = self.Utility(self.data)
+        if self.depth == 5:
+            self.evaluateValue = self.Utility(self)
             return
-        print("data--->", self.data, self.score, self.player)
+        #print("data--->", self.data, self.score, self.player)
         for l in self.NextMovePred(self.data, self.player, self.score):
             self.children.append(Node(l["data"], l["player"], l["Score"], self.depth + 1))
         for l in self.children:
-            print(" child--->", l.data, l.score, l.player)
+            pass#print(" child--->", l.data, l.score, l.player)
         for l in self.children:
             l.insert()
-
     def getNextMove(self):
-        NextMove = self.children[0]
+        pass
+    """        if not self.children : return None
+        NextMove=self.children[0]
         for l in self.children[1:]:
-            if self.player:  # AI get max  score
+            if  self.player :   #AI get max  score
                 if NextMove.beta < l.beta:
-                    NextMove = l
-            else:  # human get min score
+                    NextMove=l
+            else   :   #human get min score
                 if NextMove.alpha > l.alpha:
-                    NextMove = l
-        return NextMove
-
+                    NextMove=l
+        return NextMove"""
     def printTree(self):
-        if self._IsLeaf(self): return
-        print("data--->", self.data, self.score, self.player, self.alpha, self.beta, self.evaluateValue, self.cutoff)
+        if self._IsLeaf(self):return
+        print("data--->", self.data, self.score, self.player,self.alpha,self.beta,self.evaluateValue,self.cutoff)
         for l in self.children:
-            print(" child--->", l.data, l.score, l.player, l.alpha, l.beta, l.evaluateValue, l.cutoff)
+            print(" child--->", l.data, l.score, l.player,l.alpha,l.beta,l.evaluateValue,l.cutoff)
         for l in self.children:
             l.printTree()
-
     def Utility(self, data):
-        return random.randint(0, 500)
+        Score=0
+        Score-=sum(data.data[0:6])
+        Score += sum(data.data[6:])
+        for i,l in enumerate(data.data[0:6]):
+            if l + i > 6:
+                Score-=1
+        for i, l in enumerate(data.data[6:]):
+            if l + i > 6:
+                Score += 1
 
-    def NextMovePred(self, data, player, score):  # get list of all possible moves
+        return Score+4*(data.score[1] - data.score[0])
+
+
+    def NextMovePred(self, data, player, score): #get list of all possible moves
         MoveList = []
         ScoreInc = [0, 0]
         for i, l in zip(range(player * 6, player * 6 + 6), data[player * 6: player * 6 + 6]):
@@ -97,6 +121,11 @@ class Node:
             for _ in range(l):
                 spot = (spot + 1) % 12
                 List[spot] += 1
+            if List[spot]==1 and List[(spot+6)%12] !=0 :
+                List[spot]=0
+                spot=(spot+6)%12
+                ScoreInc[player] +=List[spot]+1
+                List[spot]=0
             node["data"] = List
             node["player"] = PT
             node["Score"] = ScoreInc
@@ -108,11 +137,31 @@ class Node:
             return True
         return False
 
+kb=board
+kp=0
+ks=[0,0]
+for i in range(1000):
+    start = time.time()
+    c = Node(kb, kp,ks)
+    c.insert()
+    c.AlphaBeta(float('-inf'),float('inf'))
+    k=c.NextMove
+    if k == None  :
+        c.score[0]+=sum(c.data[0:6])
+        c.score[1] += sum(c.data[6:])
+        c.data= [0] * 12
+        print(c.data,c.score)
+        print('GameOver')
+        if c.score[0] > c.score[1] : print("Player 1 Won")
+        else:print("Player 2 Won")
+        print(i)
+        break
 
-c = Node(board, 1, [0, 0], 0)
-c.insert()
-c.AlphaBeta(float('-inf'), float('inf'))
-print("----------------\n")
-c.printTree()
-k = c.getNextMove()
-print(k.data, k.score)
+    print(k.data,k.score,k.player)
+    if kp ==k.player :
+        pass
+    kb=k.data
+    kp=k.player
+    ks=k.score
+    end=time.time()
+    print(end-start)
