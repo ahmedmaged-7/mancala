cfd8fe7222a67253dfb5522dbb7962c9e117eb86
Bassem-Yousry <Yousrybassem@gmail.com>
2021-06-12 18:54:05 +0200

    Update NOde.py

--- NOde.py
+++ NOde.py
@@ -1,3 +1,4 @@
+from utility import Utility
 board = [4, 4, 4, 4, 4, 4,
          4, 4, 4, 4, 4, 4]
 
@@ -7,22 +8,22 @@ board = [4, 4, 4, 4, 4, 4,
 # player 1 AI
 
 class stats:
-   def __init__(self,max_depth):
-         self.cut_off=[0 for x in range(max_depth)] 
-         self.leaf_values_evaluated=[]
-         self.max_depth_explored=0 
-   def inc_cut_off(self,depth):
-         self.cut_off[depth]+=1
-         if(depth>self.max_depth_explored):
-             self.max_depth_explored=depth
-   def leaf_node_eval(self,index,score):
-           self.leaf_values_evaluated.append([index,score])
+    def __init__(self, max_depth):
+        self.cut_off = [0 for x in range(max_depth)]
+        self.leaf_values_evaluated = []
+        self.max_depth_explored = 0
 
-   def return_stats(self): 
-       if(self.leaf_values_evaluated[len(self.cut_off)-1]!=0): self.max_depth_explored=len(self.cut_off) 
-       return [self.max_depth_explored,self.leaf_values_evaluated,self.cut_off]       
+    def inc_cut_off(self, depth):
+        self.cut_off[depth] += 1
+        if (depth > self.max_depth_explored):
+            self.max_depth_explored = depth
 
+    def leaf_node_eval(self, index, score):
+        self.leaf_values_evaluated.append([index, score])
 
+    def return_stats(self):
+        if (self.leaf_values_evaluated[len(self.cut_off) - 1] != 0): self.max_depth_explored = len(self.cut_off)
+        return [self.max_depth_explored, self.leaf_values_evaluated, self.cut_off]
 
 
 class Node:
@@ -30,14 +31,13 @@ class Node:
     nonLeafNodes = 0
     cutOffs = 0
 
-    def __init__(self, data, player, score, stat,depth=0, maxDepth=1, index=None):
+    def __init__(self, data, player, score, stat, depth=0, maxDepth=1, index=None):
         Node.treeSize += 1
         Node.nonLeafNodes += 1
         # print(maxDepth)
         self.children = []
         self.index = index
         self.NextMove = None
-        self.nextMoveIndex = None
         self.alpha = float('-inf')
         self.beta = float('inf')
         self.score = score  # list of 2 contains score of each player
@@ -48,14 +48,13 @@ class Node:
         self.maxDepth = maxDepth
         self.cutoff = False
         self.leafs = 0
-        self.stats=stat
+        self.stats = stat
 
     def AlphaBeta(self, alpha, beta):
         if self._IsLeaf(self):
             Node.nonLeafNodes -= 1
             return self.evaluateValue, self.alpha, self.beta
         self.NextMove = self.children[0]
-        self.nextMoveIndex = 0
         self.alpha = alpha
         self.beta = beta
         for i, l in enumerate(self.children):
@@ -66,53 +65,35 @@ class Node:
                 self.alpha = max(value, self.alpha, betaT)
                 if l.beta > self.NextMove.beta:
                     self.NextMove = l
-                    self.nextMoveIndex = i
             else:
                 if value == None: value = float('inf')
                 if alphaT == float('-inf'): alphaT = float('inf')
                 self.beta = min(value, self.beta, alphaT)
                 if l.alpha < self.NextMove.alpha:
                     self.NextMove = l
-                    self.nextMoveIndex = i
 
             if self.alpha >= self.beta:
                 self.cutoff = True
-                Node.cutOffs+=1
+                Node.cutOffs += 1
                 Node.nonLeafNodes -= (len(self.children))
-                Node.nonLeafNodes+=(i+1)
-                
+                Node.nonLeafNodes += (i + 1)
                 self.stats.inc_cut_off(self.depth)
                 # print("cutoff")
                 break
                 # return self.evaluateValue,self.alpha,self.beta
         return self.evaluateValue, self.alpha, self.beta
 
-    def insert(self):
+    def insert(self,stealing):
         if self.depth == self.maxDepth:
             self.evaluateValue = self.Utility(self)
-            self.stats.leaf_node_eval(self.index,self.evaluateValue )
+            self.stats.leaf_node_eval(self.index, self.evaluateValue)
             return
         # print("data--->", self.data, self.score, self.player)
-        for l in self.NextMovePred(self.data, self.player, self.score):
-            self.children.append(Node(l["data"], l["player"], l["Score"], self.stats,self.depth + 1, self.maxDepth, l["index"]))
+        for l in self.NextMovePred(self.data, self.player, self.score,stealing):
+            self.children.append(
+                Node(l["data"], l["player"], l["Score"], self.stats, self.depth + 1, self.maxDepth, l["index"]))
         for l in self.children:
-            pass  # print(" child--->", l.data, l.score, l.player)
-        for l in self.children:
-            l.insert()
-
-    def getNextMove(self):
-        pass
-
-    """        if not self.children : return None
-        NextMove=self.children[0]
-        for l in self.children[1:]:
-            if  self.player :   #AI get max  score
-                if NextMove.beta < l.beta:
-                    NextMove=l
-            else   :   #human get min score
-                if NextMove.alpha > l.alpha:
-                    NextMove=l
-        return NextMove"""
+            l.insert(stealing)
 
     def printTree(self):
         if self._IsLeaf(self): return
@@ -135,7 +116,7 @@ class Node:
 
         return Score + 4 * (data.score[1] - data.score[0])
 
-    def NextMovePred(self, data, player, score):  # get list of all possible moves
+    def NextMovePred(self, data, player, score,stealing):  # get list of all possible moves
         MoveList = []
         ScoreInc = [0, 0]
         for i, l in zip(range(player * 6, player * 6 + 6), data[player * 6: player * 6 + 6]):
@@ -158,7 +139,7 @@ class Node:
             for _ in range(l):
                 spot = (spot + 1) % 12
                 List[spot] += 1
-            if List[spot] == 1 and List[(spot + 6) % 12] != 0:
+            if stealing and List[spot] == 1 and List[(spot + 6) % 12] != 0:
                 List[spot] = 0
                 spot = (spot + 6) % 12
                 ScoreInc[player] += List[spot] + 1
@@ -179,26 +160,14 @@ class Node:
 # kb=board
 # kp=0
 # ks=[0,0]
-def ai_choice(kb, ks, kp=1, maxDepth=1):
-    stat=stats(maxDepth)
-    c = Node(kb, kp, ks,stat,maxDepth=maxDepth)
-    c.insert()
+def ai_choice(kb, ks,stealing, kp=1, maxDepth=1):
+    stat = stats(maxDepth)
+    c = Node(kb, kp, ks, stat, maxDepth=maxDepth)
+    c.insert(stealing)
     c.AlphaBeta(float('-inf'), float('inf'))
     k = c.NextMove
-    #print(len(c.children))
-    if k == None:
-        c.score[0] += sum(c.data[0:6])
-        c.score[1] += sum(c.data[6:])
-        c.data = [0] * 12
-    #print(stat.return_stats())
-    #print(k.data, k.score, k.player)
-    if kp == k.player:
-        pass
-    kb = k.data
-    kp = k.player
-    ks = k.score
+    # print(len(c.children))
 
-    
 
-    return [k.index,stat.return_stats(),[Node.treeSize-1,Node.nonLeafNodes,Node.cutOffs,(Node.treeSize-1) / Node.nonLeafNodes]]
-
+    return [k.index, stat.return_stats(),
+            [Node.treeSize - 1, Node.nonLeafNodes, Node.cutOffs, (Node.treeSize - 1) / Node.nonLeafNodes]]
