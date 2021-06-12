e626cbb32be060d30183363ef7cf2a2224daedee
Bassem-Yousry <Yousrybassem@gmail.com>
2021-06-12 23:47:25 +0200

    Update utility.py

--- utility.py
+++ utility.py
@@ -1,15 +1,17 @@
-def Utility(data):
-
+def Utility(data,score):
     Score = 0
-    Score -= sum(data.data[0:6])
-    Score += sum(data.data[6:])
-
-    for i, l in enumerate(data.data[0:6]):
+    Score -= sum(data[0:6])
+    Score += sum(data[6:])
+    for i, l in enumerate(data[0:6]):
         if l + i > 6:
             Score -= 1
-
-    for i, l in enumerate(data.data[6:]):
+        elif i + l == 6:
+            Score -= 3
+    for i, l in enumerate(data[6:]):
         if l + i > 6:
             Score += 1
+        elif i + l == 6:
+            Score += 4
 
-    return Score + 4 * (data.score[1] - data.score[0])
+    return Score + 4 * (score[1] - score[0])
+
